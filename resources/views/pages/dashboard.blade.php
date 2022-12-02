05e5a5e5f9182fdf73c88ce4b3cd6fe8ebaf01fa
chirreter0 <nicolasleguizamo.es@unitropico.edu.co>
2022-12-01 22:28:08 -0500

    .

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -167,7 +167,7 @@
             data: {
                 labels: lbl,
                 datasets: [{
-                    label: "Mobile apps",
+                    label: "Mobile",
                     tension: 0.4,
                     borderWidth: 0,
                     pointRadius: 0,
